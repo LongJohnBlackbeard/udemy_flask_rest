c895d1e95b67b9012801e59142e3d70e284b5172
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-03-23 11:42:19 -0500

    debugging postgresql

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATATBASE_URI', 'sqlite:///data.db') 
+db_url = os.environ.get('DATABASE_URL')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPOGATE_EXCEPTIONS'] = True
 app.secret_key = 'daniel'
