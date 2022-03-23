bb710e08b6f72debf2877520569a7f75578cab2e
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-03-23 11:34:24 -0500

    fixed postgresql

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATATBASE_URL', 'sqlite:///data.db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATATBASE_URI', 'sqlite:///data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPOGATE_EXCEPTIONS'] = True
 app.secret_key = 'daniel'
